57df489e6d23eb601bc6c814814d15f2a31faf8b
Sergey Kulanin <sjulanin1981@gmail.com>
2023-02-19 21:58:15 +0300

    Нахождение 3 цифры

---
+++ Ex13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num=int.Parse(Console.ReadLine());
+int result=0;
+
+if(num>99)
+{
+    while(num>999)
+    {
+        num = num/10;
+    }
+result=num%10;
+Console.WriteLine($"Третья цифра {result}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
